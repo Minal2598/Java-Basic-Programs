6ad088f99568c890888f3c468e7a8602d8f1dd58
Minal <patilminalp@gmail.com>
2021-06-19 03:03:42 +0530

    Added:Utility class

--- LeapYear.java
+++ LeapYear.java
@@ -1,21 +1,14 @@
 package com.program;
 
-import java.util.Scanner;
-
-public class LeapYear {
+public class LeapYear{
 	public static void main(String[] args) {
 
-	    
-		System.out.println("enter your year::");
-	 	Scanner sc= new Scanner(System.in);
-	 	double year = sc.nextDouble();
-	 	sc.close();
+	   	System.out.println("enter your year::");
+	 	
+	 	int year = Utility.getUserSpecificInteger();
 	 	if(year>999 && year<9999)
 	 	{
 	    boolean leap = false;
-	    
-
-	 
 	    if (year % 4 == 0) {
         if (year % 100 == 0) {
 	    if (year % 400 == 0)
