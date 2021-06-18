6efdd67814ddc7617526fca9261cd8e66783de8b
Minal <patilminalp@gmail.com>
2021-06-19 02:46:13 +0530

    Added:Utility class

--- Harmonic.java
+++ Harmonic.java
@@ -1,24 +1,45 @@
-package com.program;
+  GNU nano 5.6.1                                               Harmonic.java                                                Modified
 
-import java.util.Scanner;
-public class Harmonic{
-  public static void main(String [] args){
 
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter N: ");
-    int num = sc.nextInt();
-    sc.close();
-    if(num != 0){
-      double hp = 0;  
-      for(double i=1 ; i<=num ; i++){
-        double hn = hp + 1/i;   
-        hp = hn;
-      }
-      System.out.println("Harmonic value for " +num+ "is: \n");
-      System.out.println(hp);
-    }else{
-      throw new ArithmeticException("Invalid number");
-    }
 
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package javaPrograms;
+public class HarmonicNumber{
+	public static void main(String[] args)
+	{
+	
+	double sum=0,n,i;
+	System.out.println("Please Enter the value of N:=");
+	n = Utility.getUserSpecificDouble(); 
+	for(i=1;i<=n;i++)
+	{
+	sum=sum+(1/i);
+	if(i==1)
+	System.out.println("1 + ");
+	else if(i==n)
+	System.out.println("1/ "+i);
+	else
+	System.out.println("1/ "+i);
+	}
+	System.out.println();
+	System.out.println("THE SUM OF THIS SERIES IS:="+sum);
+	}
 }
